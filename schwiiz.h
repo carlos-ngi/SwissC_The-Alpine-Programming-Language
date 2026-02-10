58fa8f36dea8fd2967ca0f199a7c269a2ed30af2
carlos_ngi <rhbniggli@gmail.com>
2026-02-10 17:57:18 +0100

    Add schwitz.h with custom type definitions

---
+++ schwiiz.h
@@ -0,0 +1,66 @@
+#ifndef SCHWIIZ_H
+#define SCHWIIZ_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// ==========================================
+// 1. PROGRAMM-BASICS
+// ==========================================
+#define hauptteil           main
+#define schluss_jetzt       return
+#define fertig_luschtig     exit
+
+// ==========================================
+// 2. DATENTYPEN (TYPES)
+// ==========================================
+#define zahl                int
+#define kommazüüg           float
+#define gnauers_züüg        double
+#define buechstabe          char
+#define nüt                 void
+#define logisch             bool
+#define langi_zahl          long
+
+// ==========================================
+// 3. KONTROLLSTRUKTUREN (LOGIC)
+// ==========================================
+#define falls               if
+#define suscht              else
+#define uswahl              switch
+#define fall                case
+#define standard            default
+#define solangs             while
+#define mach                do
+#define für_jedes           for
+#define brich_ab            break
+#define wiiter_gahts        continue
+
+// ==========================================
+// 4. LOGIK & WERTE
+// ==========================================
+#define ja_voll             true
+#define chrüzfalsch         false
+#define nüt_as_luft         NULL
+
+// ==========================================
+// 5. EIN- & AUSGABE (I/O)
+// ==========================================
+#define verzells            printf
+#define los_mal             scanf
+#define buechstabe_hole     getchar
+#define buechstabe_schriibe putchar
+
+// ==========================================
+// 6. STRUKTUREN & SPEICHER
+// ==========================================
+#define päckli              struct
+#define eigeti_art          typedef
+#define wie_gross           sizeof
+#define fixe_wärt           static
+#define fix                 const
+#define platz_haue          malloc
+#define lehre               free
+
+#endif
